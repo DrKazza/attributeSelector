7abbb1490bf26df50671535a966ad180c26a4037
DrKazza <github@drkazza.com>
2021-11-15 22:41:45 +0000

    Sting Decoding not working

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ Things to add
 6) save the number associated with that attribute
 */
 
-const fs = require("fs");
+const fs = require('fs');
+const os = require('os');
+
 
 const arrayTotal = (arr) => {
     try {
@@ -103,11 +105,34 @@ const whichBucket = (bucketBoundaries) => {
     return bucket
 }
 
+const decodeAttributesToCurrents = (currentMints, currentVariableArray) => {
+    for (i = 0; i < currentMints.length; i++) {
+        thisSerialNumber = currentMints[i].toString();
+        // the first digit is 7 ignore that
+        if (thisSerialNumber.length != 21 || thisSerialNumber.substring(0,1) != "7") {
+            console.log(`Bad serial number at entry ${i}, either doesn't lead with 7 or isn't 21 digits long`)
+            console.log(`Begins: ${thisSerialNumber.substring(0,1)}; ${thisSerialNumber.length} Long`)
+
+        } else {
+            for (j = 0; j < Math.floor(thisSerialNumber.length / 2); j++) {
+                // the variable j has two digits at j*2 + 1 and j*2 + 3
+                // the two digits make up a number k
+                // add 1 to the currentVariableArray[j][k]
+                k = parseInt(thisSerialNumber.substring(2*j+1),(2*j+3))
+                console.log(k)
+                currentVariableArray[j][k]++
+             }
+     
+        }
+    }
+    return currentVariableArray
+}
+
+
 const mintAttributes = (numberToMint) => {
 
 
     // 1) Define how many attributes there are
-    var allAttributes = []
     // Car shape * 3
     // Wheels * 4 (chromevar1, chromevar2, blackvar1, blackvar2)
     // Spoilers * 3 (none, var1, var2)
@@ -116,7 +141,8 @@ const mintAttributes = (numberToMint) => {
     // Wrap Finish * 3 (Gloss, Chrome, Satin/matte... all only appropriate for colour var 0-14)
     // Background * 15 (var0, var1, ... var14) make vars 0-7 common, 8-9 uncommon, 10-11 rare, 12-14 unique
     // Border * 4 (common, uncommon, rare, epic)
-    // NumberPlates * 25 (var0, var1, ... var24) make vars 0-5 common, 6-8 uncommon, 9-10 rare, 11-14 unique with 15-24 reserved for step 11
+    // NumberPlates * 25 (var0, var1, ... var14) make vars 0-5 common, 6-8 uncommon, 9-10 rare, 11-14 unique 
+    // GoldenNumberPlates * 10 reserved for step 12
 
     // 2) Define the max number of NFTs
     var targetIssuance = 9000
@@ -124,41 +150,72 @@ const mintAttributes = (numberToMint) => {
     // 3) Define the Max and Min for each attribute
     var carShapeMax = [4000, 4000, 4000] // 3 shapes, all common
     var carShapeMin = [2000, 2000, 2000] // 3 shapes, all common
+    var carShapeCurrent = new Array(carShapeMin.length).fill(0);
     var wheelsMax = [5000, 5000, 2000, 2000] // 4 shapes, black slightly rarer
     var wheelsMin = [3000, 3000, 500, 500] // 4 shapes, black slightly rarer
+    var wheelsCurrent = new Array(wheelsMin.length).fill(0);
     var spoilersMax = [7000, 3000, 3000] // 3 shapes, 'none' most common
     var spoilersMin = [5000, 1000, 1000] // 3 shapes, 'none' most common
+    var spoilersCurrent = new Array(spoilersMin.length).fill(0);
     var bodyTrimMax = [5000, 5000, 5000, 250, 250, 1] // 6 shapes, var4 and 5 rare var6 unique
     var bodyTrimMin = [2000, 2000, 2000, 100, 100, 1] // 6 shapes, var4 and 5 rare var6 unique
+    var bodyTrimCurrent = new Array(bodyTrimMin.length).fill(0);
     var colourMax = [2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 500, 500, 500, 500, 500, 100, 100, 100, 1, 1] // 20 shapes, vars 0-9 common, 10-14 uncommon, 15-17 rare, 18-19 unique
     var colourMin = [500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 150, 150, 150, 150, 150, 20, 20, 20, 1, 1] // 20 shapes, vars 0-8 common, 9-14 uncommon, 15-17 rare, 18-19 unique
+    var colourCurrent = new Array(colourMin.length).fill(0);
     var colourFinishMax = [4000, 4000, 4000] // 3 shapes, all common
     var colourFinishMin = [2000, 2000, 2000] // 3 shapes, all common
+    var colourFinishCurrent = new Array(colourFinishMin.length).fill(0);
     var backgroundMax = [2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 500, 500, 100, 100, 1, 1, 1] // 15 shapes, vars 0-7 common, 8-9 uncommon, 10-11 rare, 12-14 unique
     var backgroundMin = [500, 500, 500, 500, 500, 500, 500, 500, 150, 150, 20, 20, 1, 1, 1] // 15 shapes, vars 0-7 common, 8-9 uncommon, 10-11 rare, 12-14 unique
+    var backgroundCurrent = new Array(backgroundMin.length).fill(0);
     var borderMax = [7000, 4000, 1000, 400] // 4 shapes, common, uncommon, rare, epic
     var borderMin = [5000, 2000, 500, 100] // 4 shapes, common, uncommon, rare, epic
+    var borderCurrent = new Array(borderMin.length).fill(0);
     var platesMax = [3000, 3000, 3000, 3000, 3000, 3000, 500, 500, 500, 100, 100, 1, 1, 1, 1] // 15 shapes, vars 0-5 common, 6-8 uncommon, 9-10 rare, 11-14 unique 
     var platesMin = [1000, 1000, 1000, 1000, 1000, 1000, 150, 150, 150, 20, 20, 1, 1, 1, 1] // 15 shapes, vars 0-5 common, 6-8 uncommon, 9-10 rare, 11-14 unique 
+    var platesCurrent = new Array(platesMin.length).fill(0);
     var goldenPlatesMax = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] // these are the backup plates for differentiating forced epics which match all other traits
+    var goldenPlatesMin = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // these are the backup plates for differentiating forced epics which match all other traits
+    var goldenPlatesCurrent = new Array(goldenPlatesMin.length).fill(0);
+
+    var allMaxs = [carShapeMax, wheelsMax, spoilersMax, bodyTrimMax, colourMax, colourFinishMax, backgroundMax, borderMax, platesMax, goldenPlatesMax]
+    var allMins = [carShapeMin, wheelsMin, spoilersMin, bodyTrimMin, colourMin, colourFinishMin, backgroundMin, borderMin, platesMin, goldenPlatesMin]
+    var allCurrents = [carShapeCurrent, wheelsCurrent, spoilersCurrent, bodyTrimCurrent, colourCurrent, colourFinishCurrent, backgroundCurrent, borderCurrent, platesCurrent, goldenPlatesCurrent]
+
+    // Identifier would be a digit number with a max of - start with a 7 - the max will be
+    // 702,030,205,190,214,031,410
     
     // 4) read a txt file of the nft codes already minted or create one that's new if current = 0
-//    const readline = require("readline");
-    var tickerStream = fs.createWriteStream(`identifierList.txt`, {flags: 'a'});
+    try {
+        var currentMints = fs.readFileSync('identifierList.txt').toString().split(os.EOL);
+        currentMints = currentMints.map((x) => parseInt(x));
+        if (currentMints.length == 1 && isNaN(currentMints[0])) {
+            // console.log(`empty file`)
+            currentMints = [];
+        }
+    }
+    catch(err) {
+        // console.log(`no file exists`)
+        currentMints = [];
+    }
+    var writeStream = fs.createWriteStream(`identifierList.txt`, {flags: 'a'});
 
-    for (i=0; i < 1000; i++) {
-        tickerStream.write(`${String(Math.floor(Math.PI * (10 ** i)) % 10)} \n`)
+    // 5) if there are current codes already minted go through and update the current arrays
+    if (currentMints.length != 0) {
+        allCurrents = decodeAttributesToCurrents(currentMints, allCurrents);
     }
 
-    // 5) Define the number of NFTs to mint in this cycle - must be less than Final - current
-    // 6) define the array to store the nft codes
+
+    // 6) Define the number of NFTs to mint in this cycle - must be less than Final - currentMints.length
+    // 7) define the array to store the nft codes
     
-    // 7) loop thru the number of NFTs to mint 
-    // 8) choose each attribute
-    // 9) before confirming the NFT attribute combo check that it's not been used before
-    // 10) if it has go back to step 8 for a max of 10 iterations until no match is found
-    // 11) if there's still a match add use one of 10 golden attributes that are only used for this final swap - this is likely only a problem for epics at the very end of the minting process
-    // 12) Once a unique is confirmed convert to a number add it to the flat file and the "current" array for checking
+    // 8) loop thru the number of NFTs to mint 
+    // 9) choose each attribute
+    // 10) before confirming the NFT attribute combo check that it's not been used before
+    // 11) if it has go back to step 8 for a max of 10 iterations until no match is found
+    // 12) if there's still a match add use one of 10 golden attributes that are only used for this final swap - this is likely only a problem for epics at the very end of the minting process
+    // 13) Once a unique is confirmed convert to a number add it to the flat file and the "current" array for checking
 
 }
 
